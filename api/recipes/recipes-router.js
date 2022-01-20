a4e3fc88534678589f40f70fbeecd5c9045490ed
david fletcher <davidfletcheraaron@gmail.com>
2022-01-20 16:44:36 -0500

    router is routed -- on to migrations

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
+   res.json({
+      message: 'we getting '
+   })
    console.log('well this should do somethign')
    next()
 })
